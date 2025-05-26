9b196292b82b38adc4a7e25bee3babe86644fa9d
Armadon <movsisyan191287@gmail.com>
2025-05-26 13:27:40 +0400

    Clear main branch

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,34 +1,11 @@
 import "./Home.css";
-import programmer from "../../assets/programmer.jpg"
-import { useState } from "react";
 
-interface HomeProps {
-  message: string;
-  clickTesct: () => void
-}
+const Home = () => {
 
-
-const Home: React.FC <HomeProps>= ({message, clickTesct})=>{
-    const [count, setCount] = useState<number>(0);
-
-    const handleClick = () =>{
-        setCount(prevCount => prevCount +=1);
-        setCount(prevCount => prevCount +=1);
-       
-    }
-    return(
+    return (
         <>
-        <h1>{message}</h1>
-        <h1>{count}</h1>
-        <button onClick={clickTesct}>Click me</button>
-        <button onClick={handleClick}>Click me 2</button>
+            <h1>Home</h1>
         </>
-        // <div className="home-page">
-        //     <h2>Home page</h2>
-        //     <p>Welcome to our app</p>
-        //     <img src={programmer} alt="Prog" />
-        //     <img src="images/laptop.jpg" alt="Laptop" />
-        // </div>
     )
 }
 

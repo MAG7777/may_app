669b07617fd638a9e867337cf6d78fa368d12960
Armadon <movsisyan191287@gmail.com>
2025-07-14 12:54:46 +0400

    Clear main brach

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,11 @@
 import './App.css';
-import UserList from '../UserList';
-import AddUser from '../AddUser';
+
 
 function App() {
 
   return (
     <>
-      <UserList />
-      <p>...................................................</p>
-      <AddUser />
-      <p>...................................................</p>
+    <h1>Main</h1>
     </>
   )
 }
